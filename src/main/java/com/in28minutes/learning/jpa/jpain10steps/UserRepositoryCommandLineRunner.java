9bfff37e361d6d3c8d989cbf1256bfd2a82a3121
Teoh Kheng Hong <tkhenghong@gmail.com>
2020-02-24 08:17:57 +0800

    Test out more predefined methods in **Repository object

--- src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
+++ src/main/java/com/in28minutes/learning/jpa/jpain10steps/UserRepositoryCommandLineRunner.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class UserRepositoryCommandLineRunner implements CommandLineRunner {
 
@@ -21,5 +24,14 @@ public class UserRepositoryCommandLineRunner implements CommandLineRunner {
         User user = new User("Jill", "Admin");
         userRepository.save(user); // Update or Create uses the same method
         log.info("New User is created: " + user);
+
+        Optional<User> userWithIdOne = userRepository.findById(1L);
+        log.info("User is retrieved: " + userWithIdOne);
+
+        List<User> users = userRepository.findAll();
+
+        log.info("All Users : " + users);
+
+        // Tap into userRepository object to find out more methods!
     }
 }
